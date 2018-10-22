6392e82b4c372207806b6a2fadf6627a361d8397
Leon Mok <mok.leon@gmail.com>
2018-10-22 20:28:04 +0800

    Add no semicolon linting rule

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-import { Platform, Linking, NativeModules } from 'react-native';
+import { Platform, Linking, NativeModules } from 'react-native'
 
-const AppleNativePrefix = 'itms-apps://itunes.apple.com/app/id';
-const AppleWebPrefix = 'https://itunes.apple.com/app/id';
-const GooglePrefix = 'http://play.google.com/store/apps/details?id=';
-const AmazonPrefix = 'amzn://apps/android?p=';
+const AppleNativePrefix = 'itms-apps://itunes.apple.com/app/id'
+const AppleWebPrefix = 'https://itunes.apple.com/app/id'
+const GooglePrefix = 'http://play.google.com/store/apps/details?id='
+const AmazonPrefix = 'amzn://apps/android?p='
 
 export const AndroidMarket = {
   Google: 1,
   Amazon: 2,
   Other: 3,
-};
+}
 
-const noop = () => {};
+const noop = () => {}
 
 export default class Rate {
   static filterOptions(inputOptions) {
@@ -25,40 +25,40 @@ export default class Rate {
       openAppStoreIfInAppFails: true,
       inAppDelay: 3.0,
       fallbackPlatformURL: '',
-    };
+    }
     Object.keys(inputOptions).forEach((key) => {
-      options[key] = inputOptions[key];
-    });
-    return options;
+      options[key] = inputOptions[key]
+    })
+    return options
   }
 
   static rate(inputOptions, callback = noop) {
-    const options = Rate.filterOptions(inputOptions);
+    const options = Rate.filterOptions(inputOptions)
     if (Platform.OS === 'ios') {
-      options.AppleNativePrefix = AppleNativePrefix;
-      const { RNRate } = NativeModules;
+      options.AppleNativePrefix = AppleNativePrefix
+      const { RNRate } = NativeModules
       RNRate.rate(options, (response) => {
-        callback(response); // error?
-      });
+        callback(response) // error?
+      })
     } else if (Platform.OS === 'android') {
       if (options.preferredAndroidMarket === AndroidMarket.Google) {
-        Rate.openURL(GooglePrefix + options.GooglePackageName, callback);
+        Rate.openURL(GooglePrefix + options.GooglePackageName, callback)
       } else if (options.preferredAndroidMarket === AndroidMarket.Amazon) {
-        Rate.openURL(AmazonPrefix + options.AmazonPackageName, callback);
+        Rate.openURL(AmazonPrefix + options.AmazonPackageName, callback)
       } else if (options.preferredAndroidMarket === AndroidMarket.Other) {
-        Rate.openURL(options.OtherAndroidURL, callback);
+        Rate.openURL(options.OtherAndroidURL, callback)
       }
     } else {
-      Rate.openURL(options.fallbackPlatformURL, callback);
+      Rate.openURL(options.fallbackPlatformURL, callback)
     }
   }
 
   static openURL(url, callback = noop) {
     Linking.canOpenURL(url).then((supported) => {
-      callback(supported);
+      callback(supported)
       if (supported) {
-        Linking.openURL(url);
+        Linking.openURL(url)
       }
-    });
+    })
   }
 }
