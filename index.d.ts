e3cccee70320fd560981dc990c882328f62c8c7e
minhloi <jimmyloi92@yahoo.com>
2019-04-27 12:46:24 -0700

    Reformat

--- index.d.ts
+++ index.d.ts
@@ -1,21 +1,21 @@
 declare module 'react-native-rate' {
-	export interface IConfig {
-		AppleAppID?: string,
-		GooglePackageName?: string,
-		AmazonPackageName?: string,
-		OtherAndroidURL?: string,
-		preferredAndroidMarket?: AndroidMarket,
-		preferInApp?: boolean,
-		openAppStoreIfInAppFails?: boolean,
-		inAppDelay?: number,
-		fallbackPlatformURL?: string,
-	}
+  export interface IConfig {
+    AppleAppID?: string,
+    GooglePackageName?: string,
+    AmazonPackageName?: string,
+    OtherAndroidURL?: string,
+    preferredAndroidMarket?: AndroidMarket,
+    preferInApp?: boolean,
+    openAppStoreIfInAppFails?: boolean,
+    inAppDelay?: number,
+    fallbackPlatformURL?: string,
+  }
 
-	export enum AndroidMarket {
-		Google,
-		Amazon,
-		Other,
-	}
+  export enum AndroidMarket {
+    Google,
+    Amazon,
+    Other,
+  }
 
   export default class Rate {
     static rate(config: IConfig, callback: (success: boolean) => void): void;
