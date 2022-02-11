18f92b55537532c91b1a0db13302a9db80948b64
Tuan Dinh <tuandtb@fonos.vn>
2022-02-11 13:32:11 +0700

    fix import issue

--- ios/RNRate.h
+++ ios/RNRate.h
@@ -1,11 +1,5 @@
-
-#if __has_include("RCTBridgeModule.h")
-#import "RCTBridgeModule.h"
-#import "RCTConvert.h"
-#else
 #import <React/RCTBridgeModule.h>
 #import <React/RCTConvert.h>
-#endif
 #import <UIKit/UIKit.h>
 #import <StoreKit/StoreKit.h>
 
