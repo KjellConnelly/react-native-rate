c3d8f2986f85a1d889e5e3dfd518e9684158222f
Ashwin Nair <ashwin.nair.ext@proximus.com>
2023-01-16 16:06:24 +0100

    Update core dependency to latest play core review library

--- android/src/main/java/com/reactnativerate/RNRateModule.java
+++ android/src/main/java/com/reactnativerate/RNRateModule.java
@@ -12,8 +12,8 @@ import com.facebook.react.module.annotations.ReactModule;
 import com.google.android.play.core.review.ReviewInfo;
 import com.google.android.play.core.review.ReviewManager;
 import com.google.android.play.core.review.ReviewManagerFactory;
-import com.google.android.play.core.tasks.OnCompleteListener;
-import com.google.android.play.core.tasks.Task;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 
 @ReactModule(name = RNRateModule.NAME)
